2cfa8462fbf02249b761a6efa1d58bf61ea332f4
Ariadna <ariarito@hotmail.com>
2021-04-21 22:47:03 -0300

    feat: saving token

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const request = require('request');
 
 // config
-const client_id = '12b7e8ba1a5f4465b9e5c50470279cfa';
-const secret = '064090079fbe46d496161c78baa0544b';
+const client_id = '';
+const secret = '';
 const port = 3000;
 const server_url = `http://localhost:${port}`;
 const redirect_uri = `${server_url}/callback`;
